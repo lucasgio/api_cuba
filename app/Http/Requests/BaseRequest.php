6ae905c311c9d9fbef135c51568e61b91aa9f70e
Iosvany Alvarez <94980291+ialvarezDEV@users.noreply.github.com>
2022-03-03 17:00:03 -0500

    Add:config to global request

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest
+{
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            "errors" => $validator->errors()->all(),
+            "message" => "Los parámetros no son validos",
+        ],422));
+    }
+
+}
