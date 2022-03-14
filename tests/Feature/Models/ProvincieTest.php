9246002013a2823b4a4e9eebff5534ef93f24b4d
lucasgio <mikaelosoltes@gmail.com>
2022-03-14 15:09:06 -0400

    Fix:Controllers response

--- tests/Feature/Models/ProvincieTest.php
+++ tests/Feature/Models/ProvincieTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature\Models;
 
-use App\Models\Provincie;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,69 +11,37 @@ class ProvincieTest extends TestCase
 
     protected array $provincie;
 
+
     protected function setUp(): void
     {
         parent::setUp();
-
-
-      protected function setUp(): void
-      {
-          parent::setUp();
-          $this->provincie = [
-              'name' => [
-                  'Pinar del Rio',
-                  'La Habana',
-                  'Matanzas'
-              ]
-          ];
-      }
-
-
-      public function test_get_all_provincies_successfully()
-      {
-          $response = $this->getJson('/api/v1/provincies');
-          $response->assertStatus(200)
-                  ->assertJsonStructure();
-      }
-
-      public function test_store_massive_data()
-      {
-          $response = $this->postJson('/api/v1/post-provincies',$this->provincie);
-          $this->assertDatabaseHas('provincies',[
-              'name' => 'La Habana'
-          ]);
-          $response->assertStatus(201)
-                   ->assertJsonStructure();
-      }
-
-
-/*      public function test_update_provincie()
-      {
-          $id = $this->provincie['id'];
-
-          $response = $this->putJson('/api/v1/provincies/'.$id,[
-              'id' => $id,
-              'name' => 'La Habana'
-          ]);
-          $response->assertStatus(201)
-                  ->assertJsonStructure();
-      }
-
-      public function test_delete_provincie()
-      {
-          $id = $this->provincie['id'];
-          $response = $this->deleteJson('/api/v1/provincies/'.$id,['id' => $id]);
-          $response->assertStatus(200)
-          $this->provincie = Provincie::factory()->create()->toArray();
+        $this->provincie = [
+            'name' => [
+                'Pinar del Rio',
+                'La Habana',
+                'Matanzas'
+            ]
+        ];
     }
+
 
     public function test_get_all_provincies_successfully()
     {
         $response = $this->getJson('/api/v1/provincies');
         $response->assertStatus(200)
-
-                  ->assertJsonStructure();
+            ->assertJsonStructure();
     }
+
+    public function test_store_massive_data()
+    {
+        $response = $this->postJson('/api/v1/post-provincies', $this->provincie);
+        $this->assertDatabaseHas('provincies', [
+            'name' => 'La Habana'
+        ]);
+        $response->assertStatus(201)
+            ->assertJsonStructure();
+    }
+
 
     /*      public function test_update_provincie()
           {
@@ -93,13 +60,41 @@ class ProvincieTest extends TestCase
               $id = $this->provincie['id'];
               $response = $this->deleteJson('/api/v1/provincies/'.$id,['id' => $id]);
               $response->assertStatus(200)
-                      ->assertJsonStructure();
-          }
+              $this->provincie = Provincie::factory()->create()->toArray();
+        }
 
-          public function test_validation_request_provincie()
-          {
-              $response = $this->postJson('/api/v1/provincies',[]);
-              $response->assertStatus(422)
-                       ->assertJsonStructure();
-          }*/
+        public function test_get_all_provincies_successfully()
+        {
+            $response = $this->getJson('/api/v1/provincies');
+            $response->assertStatus(200)
+
+                      ->assertJsonStructure();
+        }
+
+        /*      public function test_update_provincie()
+              {
+                  $id = $this->provincie['id'];
+
+                  $response = $this->putJson('/api/v1/provincies/'.$id,[
+                      'id' => $id,
+                      'name' => 'La Habana'
+                  ]);
+                  $response->assertStatus(201)
+                          ->assertJsonStructure();
+              }
+
+              public function test_delete_provincie()
+              {
+                  $id = $this->provincie['id'];
+                  $response = $this->deleteJson('/api/v1/provincies/'.$id,['id' => $id]);
+                  $response->assertStatus(200)
+                          ->assertJsonStructure();
+              }
+
+              public function test_validation_request_provincie()
+              {
+                  $response = $this->postJson('/api/v1/provincies',[]);
+                  $response->assertStatus(422)
+                           ->assertJsonStructure();
+              }*/
 }
